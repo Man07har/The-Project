b303dd559261a1b27eb38823bb5d752f267a0316
Man07har <thakurmanoharkumar7@gmail.com>
2023-01-17 12:22:51 +0530

    The inital proposed code

---
+++ dna.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char t[] = "AAGGAG";
+    char p[] = "AG";
+    int n = sizeof(t) / sizeof(t[0]) - 1; // length of text
+    int m = sizeof(p) / sizeof(p[0]) - 1; // length of pattern
+    int window_index[n]; // array to store the starting indexes of the windows where pattern is found
+    int count = 0;
+    int num_window = 0;
+
+    while (count <= n - m) {
+        if (t[count] == p[0]) {
+            if (t[count + m - 1] == p[m - 1]) {
+                window_index[num_window] = count;
+                num_window++;
+            }
+        }
+        count++;
+    }
+
+    cout << "The pattern was found at starting indexes: ";
+    for (int i = 0; i < num_window; i++) {
+        cout << window_index[i] << " ";
+    }
+    cout << endl;
+    cout<<"\n  number of count was: "<<count<<" "<<endl;
+    return 0;
+}
