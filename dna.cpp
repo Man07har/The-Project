cffd07e85a62fc55d42fd7d756321c5ce84c9075
Man07har <thakurmanoharkumar7@gmail.com>
2023-02-10 10:49:38 +0530

    Added the mathing phase

--- dna.cpp
+++ dna.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 
 int main() {
-    char t[] = "AAGGAG";
-    char p[] = "AG";
+    string t;
+    string p;
+   cout<<"Enter the string: "<<endl;
+   cin>>t;
+   cout<<"Enter the pattern: "<<endl;
+   cin>>p;
     int n = sizeof(t) / sizeof(t[0]) - 1;
     int m = sizeof(p) / sizeof(p[0]) - 1;
     int window_index[n];
@@ -26,5 +30,24 @@ int main() {
     }
     cout << endl;
     cout<<"\n  number of count was: "<<count<<" "<<endl;
+
+    count=0;
+    int num_match=0;
+    int match_index[num_window];
+    while(count<num_window){
+        int s=window_index[count];
+       int c=1;
+       while(c<=m-2){
+        if(p[c]!=t[s+c]){
+            break;
+        }
+        c=c+1;
+       }
+    if(c=m-1){
+        match_index[num_match] =s;
+        num_match=num_match+1;
+    }
+    count=count+1;
+    }
     return 0;
 }
